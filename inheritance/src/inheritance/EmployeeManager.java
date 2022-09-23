356411b842c40f3ad4768d33e36603f5bb547244
Mahmut Ali Şahinkaya <107810135+MahmutAliSahinkaya@users.noreply.github.com>
2022-09-23 20:57:39 +0300

    Update EmployeeManager.java

--- inheritance/src/inheritance/EmployeeManager.java
+++ inheritance/src/inheritance/EmployeeManager.java
@@ -2,7 +2,7 @@ package inheritance;
 
 public class EmployeeManager extends PersonManager {
 
-	public void BestEmployee() {
+	public void bestEmployee() {
 		System.out.println("Ayın elemanı getirildi.");
 	}
 
